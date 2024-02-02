988f9ce7694cff23cf54dfd7d3ff68c56df463b4
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-02 17:57:38 +0100

    Snack 5 completato

--- Snack5/js/script.js
+++ Snack5/js/script.js
@@ -9,15 +9,18 @@ if(number >= 1 && number <= 100 && !isNaN(number)) {
     let cube = "I cubi dei numeri fino a " + number + " sono: <br>";
 
     for(let i = 1; i <= number; i++) {
+        //eseguo il cubo
         let numberCube = i ** 3;
-
-        console.log(numberCube);
 
         cube += "il cubo di " + i + " è: " + numberCube + "<br>";
 
         //stampo i cubi di ogni numero
         document.querySelector("#cube").innerHTML = cube;
     }
+
 } else {
     alert("Inserisci un valore numerico e da 1 a 100!");
+    
+    //ricarico la pagina
+    location.reload(true);
 }
