2d54aa28f3fbdfbbce04fc7241cd7df3bac06fe1
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-02 15:42:29 +0100

    Inizio programma

---
+++ Snack1/js/script.js
@@ -0,0 +1,9 @@
+//salvo due variabili contenenti le parole inserite dall'utente
+let word1 = prompt("Inserisci una parola");
+let word2 = prompt("Inserisci un'altra parola");
+
+console.log(word1);
+console.log(word2);
+
+//controllo qual'è quella più corta
+ 
