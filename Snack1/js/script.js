4cef6d22ef10538940b3899b899ee1d98bcf74f6
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-02 18:50:47 +0100

    Aggiunti controlli, completato

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,18 +1,26 @@
 //PROGRAMMA CALCOLA LUNGHEZZZA PAROLE
 
-//salvo due variabili contenenti le parole inserite dall'utente
-let word1 = prompt("Inserisci una parola");
+//salvo una variabile contenente la prima parola
+let word1 = prompt("Inserisci la prima parola");
+//salvo una variabile contenente la seconda parola
 let word2 = prompt("Inserisci un'altra parola");
 
-console.log(word1);
-console.log(word2);
+//controllo che il campo non sia vuoto
+if(word1 === "" || word2 === "") {
+    alert("Inserisci qualcosa! Non riesco a confrontare se non ho 2 parole!")
+    //ricarico la pagina
+    location.reload();
 
-//stampo le parole inserite
-document.querySelector("#word-1").innerText = word1;
-document.querySelector("#word-2").innerText = word2;
+} else {
+    //stampo le parole inserite
+    document.querySelector("#word-1").innerText = word1;
+    document.querySelector("#word-2").innerText = word2;
 
-//controllo la loro lunghezza e stampo dei risultati diversi
-if(word1.length > word2.length) {
-    document.querySelector("#word-1").innerText = word2;
-    document.querySelector("#word-2").innerText = word1;
+    //controllo la loro lunghezza e stampo dei risultati diversi
+    if(word1.length > word2.length) {
+        document.querySelector("#word-1").innerText = word2;
+        document.querySelector("#word-2").innerText = word1;
+    }
 }
+
+
