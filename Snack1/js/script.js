ce5fb5b2e4b71f6bcb858ccee3a99ab1982648b0
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-02 15:54:35 +0100

    Snack1 completato

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -1,3 +1,5 @@
+//PROGRAMMA CALCOLA LUNGHEZZZA PAROLE
+
 //salvo due variabili contenenti le parole inserite dall'utente
 let word1 = prompt("Inserisci una parola");
 let word2 = prompt("Inserisci un'altra parola");
@@ -5,5 +7,12 @@ let word2 = prompt("Inserisci un'altra parola");
 console.log(word1);
 console.log(word2);
 
-//controllo qual'è quella più corta
- 
+//stampo le parole inserite
+document.querySelector("#word-1").innerText = word1;
+document.querySelector("#word-2").innerText = word2;
+
+//controllo la loro lunghezza e stampo dei risultati diversi
+if(word1.length > word2.length) {
+    document.querySelector("#word-1").innerText = word2;
+    document.querySelector("#word-2").innerText = word1;
+}
